024d5a3090ad2d323c438ae0903d7a3b281d9811
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-05-07 00:58:45 -0500

    bridge table key

---
+++ models/Char2Item.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Player.Models
+{
+    public class Char2Item
+    {
+        public int CharacterID {get; set;} //nav property
+          public List<Character> Characters {get; set;} //pk
+
+        public int ItemID {get; set;} //mav property
+          public List<Item> Items {get; set;} //pk
+
+    }
+}
--- models/Context.cs
+++ models/Context.cs
@@ -1,16 +1,24 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace Player.Models
 {
     public class CharacterDbContext : DbContext
     {
-        public CharacterDbContext (DbContextOptions<CharacterDbContext> options)
-            : base(options)
-        {
-        }
+        public CharacterDbContext (DbContextOptions<CharacterDbContext> options) : base(options) { }
 
-        public DbSet<Player.Models.Character> Character { get; set; }
-        public DbSet<Player.Models.Spell> Spell {get; set;}
-         public DbSet<Player.Models.Item> Item {get; set;}
+        public DbSet<Player.Models.Character> Characters { get; set; }
+        public DbSet<Player.Models.Spell> Spells {get; set;}
+         public DbSet<Player.Models.Item> Items {get; set;}
+
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder) //creating bridge table composit keys
+        {
+            modelBuilder.Entity<Char2Item>().ToTable("Items");
+
+            modelBuilder.Entity<Char2Item>()
+            .HasKey(mc => new { mc.CharacterID, mc.ItemID });
+
+        } 
     }
 }
